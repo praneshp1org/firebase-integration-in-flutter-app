6a11216a9d0830c2b6c05fe37a9582a703b2423a
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-09-28 19:34:58 +0545

    some contents added

---
+++ lib/firebase_services/splash_services.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+
+import 'package:firebase_crud/ui/auth/login_screen.dart';
+import 'package:get/get.dart';
+
+class SplashService {
+  // void IsLogin(){
+  //   Timer.periodic(Duration(seconds: 3), (timer){
+  //     Get.to(LoginScreen());
+  //   });
+  // },
+  void isLogin() {
+    Timer.periodic(Duration(seconds: 3), (timer) {
+      Get.to(LoginScreen());
+    });
+  }
+}
