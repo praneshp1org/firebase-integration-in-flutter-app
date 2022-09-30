49a63a2f952e0875433bc3768986937b74f30c0c
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-09-30 14:07:07 +0545

    some contents of login with phone

---
+++ lib/ui/auth/login_with_phone_number.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginWithPhone extends StatefulWidget {
+  const LoginWithPhone({super.key});
+
+  @override
+  State<LoginWithPhone> createState() => _LoginWithPhoneState();
+}
+
+class _LoginWithPhoneState extends State<LoginWithPhone> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LOGIN '),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
