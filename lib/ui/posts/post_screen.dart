06d043100d4652b5d6bc6de5a7b40a3b906051ea
Pranesh Pyara Shrestha <80845871+praneshp1org@users.noreply.github.com>
2022-10-12 10:36:56 +0545

    same as previous

--- lib/ui/posts/post_screen.dart
+++ lib/ui/posts/post_screen.dart
@@ -26,7 +26,7 @@ class _PostScreenState extends State<PostScreen> {
       child: Scaffold(
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            Get.to(AddPostScreen());
+            Get.to(() => AddPostScreen());
           },
           child: Icon(Icons.add),
         ),
